431363721b6985096ee54f503a8c3335cc8fe593
safa A <akdiid.safa@gmail.com>
2026-02-10 20:06:57 +0100

    feat:add structure of the lab

---
+++ src/app/pages/private/student/student-layout/student-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-layout.component',
+  imports: [],
+  templateUrl: './student-layout.component.html',
+  styleUrl: './student-layout.component.css',
+})
+export class StudentLayoutComponent {
+
+}
