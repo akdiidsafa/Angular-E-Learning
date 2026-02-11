356a22c84731353be08b4717a0c0671cc313720e
hiba ait abdellah <hibaaitabdellah81@gmail.com>
2026-02-11 23:56:34 +0100

    add catalogue component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CatalogComponent } from './pages/public/catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CatalogComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
